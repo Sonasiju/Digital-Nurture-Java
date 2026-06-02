6ec5d500791b82b2779fe7994ea2286d70895c76
Sonasiju <sonasiju16@gmail.com>
2026-06-02 13:19:49 +0300

    Added Multiplication.java

---
+++ .history/Java/Multiplication_20260602131620.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Multiplication {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+     System.out.println("Enter the range");
+        int n = scanner.nextInt();
+        for (int i=1;i<=n;i++){
+            System.out.println( "num * " + i + " = " + num * i);
+        }
+        scanner.close();
+    }
+}
---
+++ Java/Multiplication.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Multiplication {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = scanner.nextInt();
+     System.out.println("Enter the range");
+        int n = scanner.nextInt();
+        for (int i=1;i<=n;i++){
+            System.out.println( num + " * " + i + " = " + (num * i));
+        }
+        scanner.close();
+    }
+}
