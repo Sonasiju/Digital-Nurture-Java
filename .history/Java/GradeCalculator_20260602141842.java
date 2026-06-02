0fffaa928ff7ce1c78dbe6b9d0b9606b641df687
Sonasiju <sonasiju16@gmail.com>
2026-06-02 14:29:27 +0300

    Added GradeCalculator.java

---
+++ .history/Java/GradeCalculator_20260602141842.java
@@ -0,0 +1,6 @@
+public class GradeCalculator {
+    public static void main(String)
+
+
+
+}
---
+++ .history/Java/GradeCalculator_20260602142556.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your marks");
+        double marks= scanner.nextDouble();
+       if (marks >= 90) {
+        System.out.println(" Grade A ");
+       }
+       else if( marks >=80)
+       {
+        System.out.println(" Grade B ");
+       }
+       else if (marks >= 70)
+       {
+        System.out.println(" Grade C ");
+       } 
+       else if (marks >= 60)
+       {
+        System.out.println(" Grade D ");
+       }
+       else 
+       {
+        System.out.println( " Grade F ");
+       }
+          scanner.close();
+       }
+
+
+
+    }
+
+
+
